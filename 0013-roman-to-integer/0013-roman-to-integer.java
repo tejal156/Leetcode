4a4a4a55237327cfaf0400201fbbfc427101c6b2
tejal156 <129065710+tejal156@users.noreply.github.com>
2024-07-05 15:07:42 +0530

    Time: 5 ms (56.84%), Space: 44.9 MB (13.24%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,45 +1,36 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character , Pair>hm = new HashMap<>() ;
-        hm.put( 'I' , new Pair( 1  , 1) ) ;
-        hm.put( 'V' , new Pair( 5  , 2) ) ;
-        hm.put( 'X' , new Pair( 10 , 3) ) ;
-        hm.put( 'L' , new Pair( 50 , 4) ) ;
-        hm.put( 'C' , new Pair( 100 , 5) ) ;
-        hm.put( 'D' , new Pair( 500 , 6) ) ;
-        hm.put( 'M' , new Pair( 1000, 7) ) ;
+        HashMap<Character , Integer>hm = new HashMap<>() ;
+        hm.put( 'I' , 1 ) ;
+        hm.put( 'V' , 5   ) ;
+        hm.put( 'X' , 10  ) ;
+        hm.put( 'L' , 50  ) ;
+        hm.put( 'C' , 100  ) ;
+        hm.put( 'D' , 500  ) ;
+        hm.put( 'M' , 1000 ) ;
 
         int i = 0 ;
         int ans = 0 ;
         while(i < s.length()-1 ){
-            int cp = hm.get(s.charAt(i) ).p ;
+            int cp = hm.get(s.charAt(i) ) ;
 
-            int np = hm.get(s.charAt(i+1) ).p ;
+            int np = hm.get(s.charAt(i+1) ) ;
             if(cp >= np){
-                ans += hm.get(s.charAt(i) ).v;
+                ans += cp;
                 i++ ;
             }
             else{
-                ans -= hm.get(s.charAt(i) ).v;
-                ans += hm.get(s.charAt(i+1) ).v;
+                ans -= cp;
+                ans += np;
                 i = i+2 ;
             }
             
         }
         if( i == s.length()-1){
-             ans += hm.get(s.charAt(i) ).v;
+             ans += hm.get(s.charAt(i) );
         }
         return ans ;
     };
 
-    class Pair {
-        int v ;
-        // char s ;
-        int p ;
-        Pair(  int v , int  p ){
-            // this.s = s ;
-            this.v = v ;
-            this.p = p ;
-        }
-    };
+
 }
