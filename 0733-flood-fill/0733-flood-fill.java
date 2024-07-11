0bb7cc00be051e3a43c65f2fa5f236a15f892d32
tejal156 <129065710+tejal156@users.noreply.github.com>
2024-07-11 12:49:58 +0530

    Time: 1 ms (43.64%), Space: 44.8 MB (55.95%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -2,9 +2,17 @@ class Solution {
     int ar[] = {0 , 0  , -1 , 1}   ; 
     int ac[] = { 1 , -1 , 0 , 0}   ; 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc] == color)return image ; 
-        helperFun(image , sr , sc , color , image[sr][sc]) ;
-        return image ; 
+        int tr = image.length ;
+        int tc = image[0].length ; 
+        int image1[][] = new int[tr][tc]  ;
+        for(int r = 0 ; r < tr ; r++){
+            for(int c = 0 ; c < tc ; c++){
+                image1[r][c] = image[r][c] ;
+            }
+        }
+        if(image[sr][sc] == color)return image1 ; 
+        helperFun(image1 , sr , sc , color , image1[sr][sc]) ;
+        return image1 ; 
     }
 
     public void helperFun(int[][] image, int sr, int sc, int color, int oldcolor) {
