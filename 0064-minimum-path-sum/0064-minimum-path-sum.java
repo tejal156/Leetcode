cdd5f951a793c2d17860312c2902260ff9851446
tejal156 <129065710+tejal156@users.noreply.github.com>
2024-07-28 19:54:58 +0530

    Time: 2 ms (68.09%), Space: 45.1 MB (84.3%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -8,20 +8,10 @@ class Solution {
     int helperFun(int grid[][] , int i , int j ,int dp[][]){
         // nr = grid.length ;
         // nc = grid[0].length ;
+        if(j >= grid[0].length || i >= grid.length)return Integer.MAX_VALUE ; 
         if(i == grid.length-1 && j == grid[0].length -1) return grid[i][j] ;
         if(dp[i][j] !=-1)return dp[i][j] ;
-        int min = Integer.MAX_VALUE ; 
-
-        // if right possible ni = i . nc = j+1
-        if(j+1 < grid[0].length){
-            min = Math.min(min , helperFun(grid,i,j+1 ,dp ) ) ;
-        }
-
-        // goes down
-        if(i+1 < grid.length){
-            min = Math.min(min , helperFun(grid,i+1,j ,dp ) ) ;
-        }
-        dp[i][j] = grid[i][j]+min ;
-        return dp[i][j] ;
+    
+        return dp[i][j] = grid[i][j] + Math.min( helperFun(grid,i,j+1 ,dp ) , helperFun(grid,i+1,j ,dp ) ) ;
     }
 }
