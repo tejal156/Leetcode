63714ba473ed8133622205dc38f9be299c384952
tejal156 <129065710+tejal156@users.noreply.github.com>
2024-07-28 20:10:19 +0530

    Time: 36 ms (17.79%), Space: 45.6 MB (9%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,31 +1,25 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int dp[][] = new int[coins.length][amount+1] ;
-        for( int r = 0 ; r < coins.length ; r++ ){
-            for( int c = 0 ; c < amount+1 ; c++ ){
-                dp[r][c] = -2 ; 
-            }
-        }
-        return helperFun( coins , amount , 0,dp) ;
+        for( int r = 0 ; r < coins.length ; r++ )for( int c = 0 ; c < amount+1 ; c++ )
+                dp[r][c] = -1 ; 
+            
+        int ans = helperFun( coins , amount , 0,dp) ;
+        return ( ans == Integer.MAX_VALUE ? -1 : ans) ;
     }
     public int helperFun(int[] coins, int ra, int ci,int dp[][] ){
         if(ra == 0) return 0 ;
-        if(ci == coins.length )return -1 ; 
-        if(dp[ci][ra] != -2) return dp[ci][ra] ; 
-        if( coins[ci] <= ra ){
-            int ac = 1+helperFun(coins , ra-coins[ci] , ci ,dp);
-
-            int nac = helperFun(coins , ra , ci+1 ,dp) ;
-             
-            if( ac == 0  ) dp[ci][ra] = nac ; 
-            else if(nac == -1)dp[ci][ra] = ac ; 
-            else dp[ci][ra] = Math.min( ac , nac ) ;
-            return dp[ci][ra]  ;
-        } 
-        else{
-            dp[ci][ra] = helperFun(coins , ra , ci+1 ,dp) ;
-            return dp[ci][ra] ; 
+        if(ci == coins.length )return Integer.MAX_VALUE ; 
+        if(dp[ci][ra] != -1) return dp[ci][ra] ; 
+        
+        int ac = Integer.MAX_VALUE ; 
+        if( coins[ci] <= ra){
+            ac = helperFun(coins , ra-coins[ci] , ci ,dp);
+            ac = ( ac == Integer.MAX_VALUE ? Integer.MAX_VALUE : ac+1  ) ; 
         }
+
+         return dp[ci][ra] = Math.min( ac ,  helperFun(coins , ra , ci+1 ,dp) ) ;    
+
         
     }
 }
