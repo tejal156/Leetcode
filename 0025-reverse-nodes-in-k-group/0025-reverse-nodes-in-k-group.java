c414f11cedfdfbe04c21a4a972c6d31b7de3be79
tejal156 <129065710+tejal156@users.noreply.github.com>
2024-07-29 23:47:20 +0530

    Time: 0 ms (100%), Space: 44.2 MB (37.05%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -10,66 +10,43 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode prev = null ;
         
-        ListNode start = head ;
-        ListNode nextE = null ;
+        ListNode start = head ; 
+        ListNode head1 = new ListNode(-1) ;
+        ListNode prev =  head1 ; 
 
         while(true){
-            ListNode end = findK( start , k) ;
-            if(end == null){
-                prev.next  = start ;
-                return head ;
+            ListNode kthNode = findk(start , k ) ;
+            if(kthNode == null ){
+                prev.next = start ; 
+                return head1.next ; 
             }
-            else if(start == head ){
-                nextE = end.next ;
-                end.next = null ;
-                head = end ;
-                reverse( start) ;
-                prev = start ;
-                start = nextE ; 
-                
+            ListNode nextNode = kthNode.next ;
+            prev.next =  kthNode ;
+            kthNode.next = null ; 
+            reverse(start) ;
+            prev = start ; 
+            start = nextNode ;
+        } 
+    }
 
-            }
-            else{
-                nextE = end.next ;
-                end.next = null ;
-                prev.next = end ;
-                reverse( start) ;
-                prev = start ;
-                start = nextE ;
-
-            }
-        }
+    void reverse(ListNode root){
+        ListNode prev = null ;
+        ListNode cn = root ;
         
+        while(cn != null){
+            ListNode next = cn.next ; 
+            cn.next = prev ; 
+            prev = cn ; 
+            cn = next ; 
+        }
     }
 
-    public ListNode findK( ListNode n , int k ){
-        for(int i = 0 ; i < k-1 ; i++ ){
-            if( n == null ){
-                return null ;
-            }
-            n = n.next ;
-        }
-        return n ;
+    ListNode findk( ListNode start ,int k ){
+        while(start!= null && k-- >1 ){start = start.next ; }
+        return start ; 
     }
 
-    public void reverse(ListNode n){
-        if(n == null || n.next == null){
-            return  ;
-        }
-        ListNode s = null ;
-        ListNode m = n ;
-        ListNode e = n.next ;
-        while( m != null){
-            m.next = s ;
-            s = m ;
-            m = e ;
-            if(e != null){
-                e= e.next ;
-            }    
-        }
 
-    }
 
 }
